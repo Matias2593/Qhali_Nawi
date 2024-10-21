a2c2ceb565b4e3a0403b3b51cb825cdcfe629063
Matias2593 <118009183+Matias2593@users.noreply.github.com>
2024-10-21 10:04:29 -0500

    Update campimetry-real-exam.jsx

--- campimetry-real-exam.jsx
+++ campimetry-real-exam.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const CampimetryRealExam = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Inicio del Examen de Campimetría</Text>
+
+      {/* WebView para mostrar el stream del ESP32 */}
+      <WebView
+        source={{ uri: 'http://10.101.48.143:81/stream' }}  // URL del ESP32
+        style={styles.webview}
+        scalesPageToFit={true}  // Ajusta el contenido a la pantalla
+      />
     </View>
   );
 };
@@ -15,13 +23,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
-    backgroundColor: '#F3F4F6', //Fondo gris claro
+    backgroundColor: '#F3F4F6',
   },
   title: {
     fontSize: 32,
     fontWeight: 'bold',
-    color: '#4C51BF', //Azul oscuro
+    color: '#4C51BF',
     textAlign: 'center',
+    marginBottom: 20,
+  },
+  webview: {
+    width: 300,
+    height: 300,
+    flex: 1, // Ajustar el contenido al WebView
   },
 });
 
